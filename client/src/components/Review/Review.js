a8943c776ce0b03ea9ea5da3ddfe87db75021889
sweetpotatozoa <tkddn3200@korea.ac.kr>
2024-07-15 03:17:50 +0900

    feat : 마이페이지 css

--- client/src/components/Review/Review.js
+++ client/src/components/Review/Review.js
@@ -18,18 +18,35 @@ const Review = ({
   return (
     <div key={entry._id} className={styles.reviewEntry}>
       <div onClick={() => handleEntryClick(entry._id)}>
-        <div>{dayDifference}일차 독서일지</div>
-        <h3>{entry.title}</h3>
-        <small>{reviewDate.format('YYYY-MM-DD')}</small>
-        <button onClick={() => handleEditClick(entry._id)}>수정하기</button>
-        <p>
-          {entry.expanded ? entry.content : truncateContent(entry.content, 150)}
-        </p>
-        <div>
-          {entry.startPage}p~{entry.endPage}p
+        <div className={styles.header}>
+          <div className={styles.day}>{dayDifference}일차 독서기록</div>
+          <div className={styles.page}>
+            {entry.startPage}p~{entry.endPage}p
+          </div>
         </div>
-        <div>❤ {entry.likedBy.length}개</div>
-        <div>□ {entry.comments.length}개</div>
+
+        <div className={styles.content}>
+          <div className={styles.contentInfo}>
+            <div className={styles.top}>
+              <h3>{entry.title}</h3>
+              <small>{reviewDate.format('YYYY.MM.DD')}</small>
+            </div>
+            <button onClick={() => handleEditClick(entry._id)}>수정하기</button>
+          </div>
+          <p className={styles.main}>
+            {entry.expanded
+              ? entry.content
+              : truncateContent(entry.content, 150)}
+          </p>
+          <div className={styles.bottom}>
+            <div>
+              <img src='/images/Heart_01.svg'></img> {entry.likedBy.length}개
+            </div>
+            <div>
+              <img src='/images/Chat.svg'></img> {entry.comments.length}개
+            </div>
+          </div>
+        </div>
       </div>
       {entry.expanded && (
         <div className={styles.commentsSection}>
