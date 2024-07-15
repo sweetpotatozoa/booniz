dcb623fd90f6ca3b0124223dcc7ad18357112d5d
sweetpotatozoa <tkddn3200@korea.ac.kr>
2024-07-15 16:10:32 +0900

    일지작성 페이지 디자인

--- client/src/components/WriteForm.js/WriteForm.js
+++ client/src/components/WriteForm.js/WriteForm.js
@@ -3,44 +3,54 @@ import styles from './WriteForm.module.css'
 
 const WriteForm = ({ formData, handleChange, handleSubmit, error }) => {
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <h1>오늘은 독서 일지를 작성해볼까요?</h1>
-      <div>
-        <input
-          type='text'
-          name='startPage'
-          value={formData.startPage}
-          onChange={handleChange}
-        />
-        <span>페이지</span> <span>~</span>
-        <input
-          type='text'
-          name='endPage'
-          value={formData.endPage}
-          onChange={handleChange}
-        />
-        <span>페이지</span>
+      <div className={styles.pages}>
+        읽은 쪽수
+        <div className={styles.page}>
+          <input
+            type='text'
+            name='startPage'
+            value={formData.startPage}
+            onChange={handleChange}
+          />
+          <span>페이지</span> <span>~</span>
+          <input
+            type='text'
+            name='endPage'
+            value={formData.endPage}
+            onChange={handleChange}
+          />
+          <span>페이지</span>
+        </div>
       </div>
-      <div>
-        <div>제목</div>
+      <div className={styles.container}>
+        <div className={styles.title}>제목</div>
         <input
           type='text'
           name='title'
           value={formData.title}
           onChange={handleChange}
+          className={styles.input}
         />
       </div>
-      <div>
-        <div>본문</div>
+      <div className={styles.container}>
+        <div className={styles.title}>본문</div>
         <textarea
           name='content'
           value={formData.content}
           onChange={handleChange}
+          className={styles.input}
+          style={{ height: '176px' }}
         />
       </div>
-      <small>* 300자 이상 작성해야 해요!</small>
-      {error && <p className={styles.error}>{error}</p>}
-      <button type='submit'>업로드 하기</button>
+      <div className={styles.bottom}>
+        <div className={styles.text} id='text'>
+          <div className={styles.detail}>* 300자 이상 작성해야 해요!</div>
+          {error ? <div className={styles.error}>{error}</div> : null}
+        </div>
+        <button type='submit'>업로드 하기</button>
+      </div>
     </form>
   )
 }
